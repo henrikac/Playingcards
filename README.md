b4dff83142d5f23c586de80b3979ed6f48009b82
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-07 20:18:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Playing Cards
+
+My 10th imperative programming assignment
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Programmer: Henrik A. Christensen     Date Completed: in progress
+ * Instructor: Kurt Nørmark              Class:          Imperative Programming
+ *
+ * Coming soon...
+*/
+
+#include <stdlib.h>
+
+int main(void)
+{
+  return EXIT_SUCCESS;
+}
