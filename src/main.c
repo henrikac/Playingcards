f2bba1789fd24ba5585ead3b36548d4881b88dcd
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-08 10:48:09 +0100

    added types suit, rank and card

--- src/main.c
+++ src/main.c
@@ -7,6 +7,14 @@
 
 #include <stdlib.h>
 
+typedef enum { hearts, diamonds, clovers, spades } suit;
+typedef enum { TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, J, Q, K, A } rank;
+
+typedef struct {
+  suit kuloer;
+  rank vaerdi;
+} card;
+
 int main(void)
 {
   return EXIT_SUCCESS;
