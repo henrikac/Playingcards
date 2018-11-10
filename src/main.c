316c50a8c0ce3c9b39e98770b797007bcb1afb3d
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-10 18:26:43 +0100

    refactor cmp_cards

--- src/main.c
+++ src/main.c
@@ -212,19 +212,12 @@ int cmp_cards(const void *c1, const void *c2)
   card *card1 = (card*)c1,
        *card2 = (card*)c2;
 
-  if (card1->suit < card2->suit)
+  if (card1->suit < card2->suit || (card1->suit == card2->suit && card1->rank < card2->rank))
     return -1;
-  else if (card1->suit > card2->suit)
+  else if (card1->suit > card2->suit || (card1->suit == card2->suit && card1->rank > card2->rank))
     return 1;
   else
-  {
-    if (card1->rank < card2->rank)
-      return -1;
-    else if (card1->rank > card2->rank)
-      return 1;
-    else
-      return 0;
-  }
+    return 0;
 }
 
 /**
